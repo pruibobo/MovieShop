32f0172232dfe311221d82d341f7fa5297f3b7d3
Peibin Rui <ruipeibin1998@gmail.com>
2021-11-08 22:03:51 -0700

    Finished Creating Entities

---
+++ ApplicationCore/Entities/Trailer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Trailer
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public string? TrailerUrl { get; set; }
+        public string? Name { get; set; }
+
+        // Navigation Properties
+        public Movie Movie { get; set; }
+    }
+}

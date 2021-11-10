2fc65f11a950af1cd53b053ed6e8352259d658d6
Peibin Rui <ruipeibin1998@gmail.com>
2021-11-10 01:02:41 -0700

    Created Request/Response Models

---
+++ ApplicationCore/Models/CastResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Models
+{
+    public class CastReponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string TmdnUrl { get; set; }
+        public string ImdbUrl { get; set; }
+        public string ProfilePath { get; set; }
+        public string Character { get; set; }
+    }
+}
---
+++ ApplicationCore/Models/UserRegisterRequestModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Models
+{
+    public class UserRegisterRequestModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100,ErrorMessage = "The Password should be minimum of 8 characters " +
+                                         "and maximum of 100 character",MinimumLength = 8)]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()]).{8,}$",
+            ErrorMessage = "Password should have minimum of 8 characters and should " +
+                           "include one upper, lower, number and a special character")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required] 
+        [StringLength(100)]
+        public string FirstName { get; set; }
+        [Required] 
+        [StringLength(100)]
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
